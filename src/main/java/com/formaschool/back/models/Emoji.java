bd1394bf172843a95fe0bddb4aec6ccf936d10a6
Jason Vennin <jason.vennin@gmail.com>
2021-06-08 14:20:22 +0200

    feat(emoji): modif model

--- src/main/java/com/formaschool/back/models/Emoji.java
+++ src/main/java/com/formaschool/back/models/Emoji.java
@@ -1,6 +1,7 @@
 package com.formaschool.back.models;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -15,11 +16,18 @@ public class Emoji {
 
 	@Id
 	private String id;
+	
+	@DBRef
+	private User user;
+	@DBRef
+	private Team team;
 
 	private String name;
 	private String picture;
 
-	public Emoji(String name, String picture) {
+	public Emoji(User user, Team team, String name, String picture) {
+		this.user = user;
+		this.team = team;
 		this.name = name;
 		this.picture = picture;
 	}
